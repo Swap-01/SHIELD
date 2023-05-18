49e6ce9f0e221c9cd0eb92b7eb74f0d2291e9a8f
Amit <daluiamit.19@gmail.com>
2023-05-18 20:01:51 +0530

    chnage the url

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose= require("mongoose");
 
-mongoose.connect(process.env.MONGO_URL + process.env.MONGO_DATABASE, {
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology:true,
 }).then(()=>{
